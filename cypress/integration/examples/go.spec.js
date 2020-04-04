4f34a46f82e648080291b5bbaace5594c26817c2
ya-che.li <ya-che.li@and.digital>
2020-04-04 15:10:49 +0100

    fetch the image

--- cypress/integration/examples/go.spec.js
+++ cypress/integration/examples/go.spec.js
@@ -3,5 +3,9 @@ context('risu Crawl', () => {
         cy.visit('https://risu.io/5qN');
         cy.get(".form-control").type("0710");
         cy.get(".input-group-append > .btn").click();
+        cy.log('Login successful');
+        cy.get("img").invoke("attr", "src").then(src=>{
+            cy.writeFile('./go.txt', src);
+        });
     });
 });
